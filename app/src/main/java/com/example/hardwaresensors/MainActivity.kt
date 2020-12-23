41faadab931864c0c799df70b179eb89327b0a94
vmehra25 <vmehra25@yahoo.com>
2020-12-23 22:30:20 +0530

    proximity sensor

--- app/src/main/java/com/example/hardwaresensors/MainActivity.kt
+++ app/src/main/java/com/example/hardwaresensors/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.hardwaresensors
 
 import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,11 +12,46 @@ import androidx.core.content.getSystemService
 import java.lang.UnsupportedOperationException
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var sensorEventListener:SensorEventListener
+    lateinit var sensorManager: SensorManager
+    lateinit var proximitySensor: Sensor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val sensorManager = getSystemService<SensorManager>()
 
+        sensorManager = getSystemService<SensorManager>()!!
+
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+
+        sensorEventListener = object :SensorEventListener{
+            override fun onSensorChanged(event: SensorEvent?) {
+                Log.d("SNSCHANGE", """
+                    onSensorChange: ${event!!.values[0]}
+                """.trimIndent())
+            }
+
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+            }
+        }
 
     }
+
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(
+                sensorEventListener,
+                proximitySensor,
+                1000 * 1000
+        )
+    }
+
+    override fun onPause() {
+        sensorManager.unregisterListener(sensorEventListener)
+        super.onPause()
+    }
+
 }

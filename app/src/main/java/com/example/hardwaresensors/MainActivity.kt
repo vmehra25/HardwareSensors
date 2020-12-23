b81c8d33429e0c2178fd16b03d9582140cbba7e5
vmehra25 <vmehra25@yahoo.com>
2020-12-23 18:02:56 +0530

    initial commit

---
+++ app/src/main/java/com/example/hardwaresensors/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.hardwaresensors
+
+import android.hardware.Sensor
+import android.hardware.SensorManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.content.getSystemService
+import java.lang.UnsupportedOperationException
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val sensorManager = getSystemService<SensorManager>()
+
+
+    }
+}

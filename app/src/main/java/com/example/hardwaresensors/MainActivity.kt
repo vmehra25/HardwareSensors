4a1b2a1a74f30eeb23aade98a719203829ebca4a
vmehra25 <vmehra25@yahoo.com>
2020-12-24 16:43:38 +0530

    added accelerometer functionality

--- app/src/main/java/com/example/hardwaresensors/MainActivity.kt
+++ app/src/main/java/com/example/hardwaresensors/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.hardwaresensors
 
+import android.graphics.Color
+import android.graphics.ColorSpace
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -9,13 +11,19 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.core.content.getSystemService
+import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.UnsupportedOperationException
+import kotlin.math.roundToInt
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var sensorEventListener:SensorEventListener
     lateinit var sensorManager: SensorManager
     lateinit var proximitySensor: Sensor
+    private lateinit var accelSensor: Sensor
+
+    val colors = arrayOf(Color.RED, Color.BLUE, Color.CYAN, Color.MAGENTA, Color.YELLOW)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,12 +32,21 @@ class MainActivity : AppCompatActivity() {
         sensorManager = getSystemService<SensorManager>()!!
 
         proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+        accelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 
         sensorEventListener = object :SensorEventListener{
             override fun onSensorChanged(event: SensorEvent?) {
-                Log.d("SNSCHANGE", """
-                    onSensorChange: ${event!!.values[0]}
-                """.trimIndent())
+                if(event?.sensor?.type == Sensor.TYPE_PROXIMITY){
+                    if(event!!.values[0] > 0){
+                        flProximityIndicator.setBackgroundColor(colors[Random.nextInt(5)])
+                    }
+                    Log.d("SNSCHANGE", """
+                        onSensorChange: ${event!!.values[0]}
+                        """.trimIndent())
+                }
+                if(event?.sensor?.type == Sensor.TYPE_ACCELEROMETER){
+                    flAccelerometerIndicator.setBackgroundColor(Color.rgb(accellToColor(event!!.values[0]), accellToColor(event!!.values[1]), accellToColor(event!!.values[2])))
+                }
             }
 
             override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
@@ -44,14 +61,17 @@ class MainActivity : AppCompatActivity() {
         super.onResume()
         sensorManager.registerListener(
                 sensorEventListener,
-                proximitySensor,
+                accelSensor,
                 1000 * 1000
         )
+
     }
 
     override fun onPause() {
         sensorManager.unregisterListener(sensorEventListener)
         super.onPause()
     }
+
+    private fun accellToColor(accell:Float) = (((accell + 12) / 24) * 255).roundToInt()
 
 }
